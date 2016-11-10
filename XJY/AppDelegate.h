e91722e8170e0618276cce015f34ceaa892397f4
teacher <teacher@teacherdeMac.local>
2016-11-10 09:49:13 +0800

    Initial Commit

---
+++ XJY/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XJY
+//
+//  Created by teacher on 16-11-10.
+//  Copyright (c) 2016年 teacher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XJY/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XJY
+//
+//  Created by teacher on 16-11-10.
+//  Copyright (c) 2016年 teacher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
